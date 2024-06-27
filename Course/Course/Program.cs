e734e816001b5cb737c0c9d5008b460b4c1e6731
silveiravit <vitorsilveira17@outlook.com>
2024-06-26 22:38:57 -0300

    Primeiros passos no C-Sharp

---
+++ Course/Course/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase);
+        }
+    }
+}
